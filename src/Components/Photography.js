74ece6d23a95f9a9eb3ab4fbf02042ba965d4847
Justin Zhao <justin@justinzhao.net>
2022-09-13 01:51:47 -0400

    finished photography site link

--- src/Components/Photography.js
+++ src/Components/Photography.js
@@ -1,10 +1,14 @@
 import Box from "@mui/material/Box";
 import Stack from "@mui/material/Stack";
 import Typography from "@mui/material/Typography";
+import Card from "@mui/material/Card";
+import Grid from "@mui/material/Grid";
+import CardActionArea from "@mui/material/CardActionArea";
+import Paper from "@mui/material/Paper";
 
 import Divider from "../Util/Divider";
 
-import BuildOutlinedIcon from "@mui/icons-material/BuildOutlined";
+import CollectionsOutlinedIcon from "@mui/icons-material/CollectionsOutlined";
 
 export default function Photography() {
   return (
@@ -12,11 +16,52 @@ export default function Photography() {
       {/* TITLE */}
       <Divider />
       <Stack direction="row" gap={2} alignItems="center">
-        <BuildOutlinedIcon />
+        <CollectionsOutlinedIcon />
         <Typography color="primary" variant="title" sx={{ pt: 0.1 }}>
-          Under Construction...
+          Photography Site
         </Typography>
       </Stack>
+
+      {/* INTRO */}
+      <Box sx={{ pt: 7 }}>
+        <Typography color="primary" variant="body1">
+          Photography is one of the hobbies that I picked up during Covid
+          quarantine. I got my first camera (Fujifilm xt-30), and have been
+          shooting with it ever since.
+        </Typography>
+      </Box>
+
+      {/* LINK */}
+      <Box sx={{ pt: { xs: 4, sm: 7 } }}>
+        <Card>
+          <CardActionArea
+            disableFocusRipple
+            onClick={() => {
+              window.open("http://photography.justinzhao.net", "_blank");
+            }}
+          >
+            <Paper sx={{ p: { xs: 3, sm: 2 } }}>
+              <Box
+                component="img"
+                alt="Me"
+                src="/assets/photography.png"
+                sx={{
+                  width: "100%",
+                  borderRadius: 1,
+                }}
+              ></Box>
+              <Box sx={{ pt: 2 }}>
+                <Typography color="primary" variant="body1">
+                  http://photography.justinzhao.net
+                </Typography>
+                <Typography color="primary" variant="body2" sx={{ pt: 3 }}>
+                  See some of the photos I've taken over the years here!
+                </Typography>
+              </Box>
+            </Paper>
+          </CardActionArea>
+        </Card>
+      </Box>
     </Box>
   );
 }
